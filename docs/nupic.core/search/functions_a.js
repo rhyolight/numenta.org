9d2955fbc0192c20a794e9f9538343b43a44edfb
Numenta CI <ci@numenta.org>
2014-06-30 19:11:53 +0000

    NuPIC Doxygen automated doc build.

--- docs/nupic.core/search/functions_a.js
+++ docs/nupic.core/search/functions_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['mappotential1d_5f',['mapPotential1D_',['../classnta_1_1algorithms_1_1spatial__pooler_1_1_spatial_pooler.html#ac861fd2a32c2df4a5ac198367fb0e9b6',1,'nta::algorithms::spatial_pooler::SpatialPooler']]]
+  ['mapcolumn_5f',['mapColumn_',['../classnta_1_1algorithms_1_1spatial__pooler_1_1_spatial_pooler.html#a08fed3797d6c30b898b9a798bb70094d',1,'nta::algorithms::spatial_pooler::SpatialPooler']]],
+  ['mappotential_5f',['mapPotential_',['../classnta_1_1algorithms_1_1spatial__pooler_1_1_spatial_pooler.html#abde1873c29f01b04ce7ae65aaf604142',1,'nta::algorithms::spatial_pooler::SpatialPooler']]]
 ];
