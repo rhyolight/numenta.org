82b0bb6ee26e32614262092e2b47b484f4ad11f6
Brev Patterson <me@brev.name>
2015-04-28 16:55:23 -0700

    WIP #172 Everything but webfonts working now.

--- assets/js/base.js
+++ assets/js/base.js
@@ -4,6 +4,21 @@
 
 'use strict';
 
+
+// requires
+
+var jquery = require('jquery');
+window.$ = jquery;
+window.jQuery = jquery;
+
+require('bootstrap-sass/assets/javascripts/bootstrap');
+
+var platform = require('platform/platform');
+window.platform =  platform;
+
+
+// methods
+
 (function () {
   $(document).ready(function() {
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 var checkpages = require('check-pages');
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var webpack = require('gulp-webpack');
+
+var port = process.env.TEST_PORT || 8008;
 
 
 gulp.task('serve', function (callback) {
-  gulp.src('_site/').pipe(webserver({ port: 8008 }));
+  gulp.src('_site/').pipe(webserver({ port: port }));
   callback();
 });
 
 gulp.task('checkpages', [ 'serve' ], function (callback) {
     var options = {
-      pageUrls:         [ 'http://localhost:8008/' ],
+      pageUrls:         [ 'http://localhost:' + port ],
       checkLinks:       true,
       onlySameDomain:   true,
       queryHashes:      true,
@@ -29,7 +32,21 @@ gulp.task('checkpages', [ 'serve' ], function (callback) {
   checkpages(console, options, callback);
 });
 
+gulp.task('webpack', function () {
+  return gulp.
+    src('assets/js/base.js').
+    pipe(webpack({
+      module: {
+        loaders: []
+      },
+      output: {
+        filename: 'index.js'
+      }
+    })).
+    pipe(gulp.dest('assets/js/bundle/'));
+});
 
-gulp.task('default');
+
+gulp.task('default', [], function () {});
 
 gulp.task('check', [ 'serve', 'checkpages' ]);
