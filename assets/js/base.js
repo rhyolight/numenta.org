d80db1b87b0d0c9bd1ac370aa6b84e5029e57de0
Brev Patterson <me@brev.name>
2015-04-28 18:39:29 -0700

    WIP #172 backtracking on webpack.

--- assets/js/base.js
+++ assets/js/base.js
@@ -4,26 +4,11 @@
 
 'use strict';
 
-
-// requires
-
-var jquery = require('jquery');
-window.$ = jquery;
-window.jQuery = jquery;
-
-require('bootstrap-sass/assets/javascripts/bootstrap');
-
-var platform = require('platform/platform');
-window.platform =  platform;
-
-
-// methods
-
 (function () {
-  $(document).ready(function() {
+  $(document).ready(function () {
 
     // Back-to-top functionality
-    $('#back-to-top').on('click', function(event) {
+    $('#back-to-top').on('click', function (event) {
       event.preventDefault();
       $('html, body').animate({ scrollTop: '0px' });
     });
